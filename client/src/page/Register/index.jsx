d4f0562512ad6c22d02364c223671b8faf776101
Andres Jaramillo <JaramilloG1999@outlook.com>
2022-03-26 16:44:27 -0500

    feat: styles globals and update structure register

--- client/src/page/Register/index.jsx
+++ client/src/page/Register/index.jsx
@@ -1,7 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import register from "../../assets/img/register.svg";
 const Register = () => {
+  const [newUser, setNewUser] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
   return (
     <div className="container-1400 h-screen m-auto flex justify-center items-center">
       <div className="lg:w-2/5 hidden lg:block">
@@ -10,22 +15,20 @@ const Register = () => {
       <div className="h-full md:xl:lg:w-6/12 flex justify-center items-center">
         <div className="m-auto py-10 px-10 sm:p-20 xl:w-10/12 bg-white shadow">
           <div className="space-y-4 text-center">
-            <p className="font-medium text-lg text-gray-600">
-              Sign Up!
-            </p>
+            <p className="font-medium text-lg text-gray-600">Sign Up!</p>
           </div>
 
-          <div role="hidden" className="mt-12 border-t">
-            <span className="block w-max mx-auto -mt-3 px-4 text-center text-gray-500 bg-white">
-              Or
-            </span>
-          </div>
+          <div role="hidden" className="mt-12 border-t" />
 
           <form action="" className="space-y-6 py-6">
             <div className="space-y-6">
               <input
                 type="text"
                 placeholder="Your Full Name"
+                value={newUser.name}
+                onChange={(e) =>
+                  setNewUser({ ...newUser, name: e.target.value })
+                }
                 className="w-full py-3 px-6 ring-1 ring-gray-300 rounded-xl placeholder-gray-600 bg-transparent transition disabled:ring-gray-200 disabled:bg-gray-100 disabled:placeholder-gray-400 invalid:ring-red-400 focus:invalid:outline-none"
               />
             </div>
@@ -34,6 +37,10 @@ const Register = () => {
               <input
                 type="email"
                 placeholder="Your Email"
+                value={newUser.email}
+                onChange={(e) =>
+                  setNewUser({ ...newUser, email: e.target.value })
+                }
                 className="w-full py-3 px-6 ring-1 ring-gray-300 rounded-xl placeholder-gray-600 bg-transparent transition disabled:ring-gray-200 disabled:bg-gray-100 disabled:placeholder-gray-400 invalid:ring-red-400 focus:invalid:outline-none"
               />
             </div>
@@ -42,6 +49,10 @@ const Register = () => {
               <input
                 type="password"
                 placeholder="Your Password"
+                value={newUser.password}
+                onChange={(e) =>
+                  setNewUser({ ...newUser, password: e.target.value })
+                }
                 className="w-full py-3 px-6 ring-1 ring-gray-300 rounded-xl placeholder-gray-600 bg-transparent transition disabled:ring-gray-200 disabled:bg-gray-100 disabled:placeholder-gray-400 invalid:ring-red-400 focus:invalid:outline-none"
               />
             </div>
@@ -58,6 +69,7 @@ const Register = () => {
                 </span>
               </Link>
             </div>
+            <div role="hidden" className="mt-12 border-t" />
           </form>
         </div>
       </div>
