f440157136a34aaca23fdd327393dc80fd7f81f7
Andres Jaramillo <JaramilloG1999@outlook.com>
2022-03-26 14:26:21 -0500

    feat: structure login and register

--- client/src/page/Login/index.jsx
+++ client/src/page/Login/index.jsx
@@ -1,6 +1,5 @@
 import "./styles.scss";
 import React from "react";
-import logo from "../../assets/img/logo.png";
 import welcome from "../../assets/img/welcome.svg";
 import { Link } from "react-router-dom";
 
@@ -8,21 +7,17 @@ const Login = () => {
   return (
     <div className="container-1400 h-screen m-auto flex justify-center items-center">
       <div className="lg:w-2/5 hidden lg:block">
-        <img src={welcome} alt="welcome-img"/>
+        <img src={welcome} alt="welcome-img" />
       </div>
       <div className="h-full md:xl:lg:w-6/12 flex justify-center items-center">
         <div className="m-auto py-10 px-10 sm:p-20 xl:w-10/12 bg-white shadow">
           <div className="space-y-4 text-center">
             <p className="font-medium text-lg text-gray-600">
-              Welcome to Hass Colombia!
+              Log in Hass Colombia!
             </p>
           </div>
 
-          <div role="hidden" className="mt-12 border-t">
-            <span className="block w-max mx-auto -mt-3 px-4 text-center text-gray-500 bg-white">
-              Or
-            </span>
-          </div>
+          <div role="hidden" className="mt-12 border-t" />
 
           <form action="" className="space-y-6 py-6">
             <div>
@@ -45,12 +40,13 @@ const Login = () => {
               <button className="w-full px-6 py-3 rounded-xl bg-indigo-600 transition hover:bg-indigo-700 mb-5">
                 <span className="font-semibold text-white text-lg">Login</span>
               </button>
-              <Link to="/register" className="w-max p-3 -ml-3">
+              <Link to="/register">
                 <span className="text-sm tracking-wide text-blue-600 hover:text-blue-700">
                   Don't have an account with us?
                 </span>
               </Link>
             </div>
+            <div role="hidden" className="mt-12 border-t" />
           </form>
         </div>
       </div>
--- client/src/page/Register/index.jsx
+++ client/src/page/Register/index.jsx
@@ -1,9 +1,68 @@
-import React from 'react'
-
+import React from "react";
+import { Link } from "react-router-dom";
+import register from "../../assets/img/register.svg";
 const Register = () => {
   return (
-    <div>Register</div>
-  )
-}
+    <div className="container-1400 h-screen m-auto flex justify-center items-center">
+      <div className="lg:w-2/5 hidden lg:block">
+        <img src={register} alt="register-img" />
+      </div>
+      <div className="h-full md:xl:lg:w-6/12 flex justify-center items-center">
+        <div className="m-auto py-10 px-10 sm:p-20 xl:w-10/12 bg-white shadow">
+          <div className="space-y-4 text-center">
+            <p className="font-medium text-lg text-gray-600">
+              Sign Up!
+            </p>
+          </div>
 
-export default Register
+          <div role="hidden" className="mt-12 border-t">
+            <span className="block w-max mx-auto -mt-3 px-4 text-center text-gray-500 bg-white">
+              Or
+            </span>
+          </div>
+
+          <form action="" className="space-y-6 py-6">
+            <div className="space-y-6">
+              <input
+                type="text"
+                placeholder="Your Full Name"
+                className="w-full py-3 px-6 ring-1 ring-gray-300 rounded-xl placeholder-gray-600 bg-transparent transition disabled:ring-gray-200 disabled:bg-gray-100 disabled:placeholder-gray-400 invalid:ring-red-400 focus:invalid:outline-none"
+              />
+            </div>
+
+            <div>
+              <input
+                type="email"
+                placeholder="Your Email"
+                className="w-full py-3 px-6 ring-1 ring-gray-300 rounded-xl placeholder-gray-600 bg-transparent transition disabled:ring-gray-200 disabled:bg-gray-100 disabled:placeholder-gray-400 invalid:ring-red-400 focus:invalid:outline-none"
+              />
+            </div>
+
+            <div className="space-y-6">
+              <input
+                type="password"
+                placeholder="Your Password"
+                className="w-full py-3 px-6 ring-1 ring-gray-300 rounded-xl placeholder-gray-600 bg-transparent transition disabled:ring-gray-200 disabled:bg-gray-100 disabled:placeholder-gray-400 invalid:ring-red-400 focus:invalid:outline-none"
+              />
+            </div>
+
+            <div>
+              <button className="w-full px-6 py-3 rounded-xl bg-indigo-600 transition hover:bg-indigo-700 mb-5">
+                <span className="font-semibold text-white text-lg">
+                  Register
+                </span>
+              </button>
+              <Link to="/" className="w-max p-3 -ml-3">
+                <span className="text-sm tracking-wide text-blue-600 hover:text-blue-700">
+                  Do you already have an account with us?
+                </span>
+              </Link>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Register;
